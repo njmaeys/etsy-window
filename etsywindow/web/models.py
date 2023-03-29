b476a30a573be8cc74ce53a16304febc33afc9dc
njmaeys <nmaeys@hotmail.com>
2023-03-29 17:30:50 -0500

    model update

--- etsywindow/web/models.py
+++ etsywindow/web/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Account(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True)
     store_name = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_created=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class User(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True)
@@ -11,8 +14,12 @@ class User(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     email_address = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_created=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Listing(models.Model):
     listing_id = models.CharField(primary_key=True, max_length=250) # This is the id from Etsy
     account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
     image_url = models.CharField(max_length=250) # This is the id from Etsy
+    created_at = models.DateTimeField(auto_created=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
