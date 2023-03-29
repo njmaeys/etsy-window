be8472539a5357326d1cbdf7cce8cce8994fa933
njmaeys <nmaeys@hotmail.com>
2023-03-29 17:05:03 -0500

    initial DB structure and admin page setup

--- etsywindow/web/admin.py
+++ etsywindow/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Account, User, Listing
 
-# Register your models here.
+admin.site.register([
+    Account,
+    User,
+    Listing,
+])
--- etsywindow/web/models.py
+++ etsywindow/web/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    store_name = models.CharField(max_length=250)
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email_address = models.CharField(max_length=250)
+
+class Listing(models.Model):
+    listing_id = models.CharField(primary_key=True, max_length=250) # This is the id from Etsy
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=250) # This is the id from Etsy
