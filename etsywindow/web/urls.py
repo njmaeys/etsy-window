61de88b8f9b18438f645f27b45898fe364c02d52
njmaeys <nmaeys@hotmail.com>
2023-04-02 08:54:45 -0500

    need to fix some sign up form stuff

--- etsywindow/web/urls.py
+++ etsywindow/web/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views.index import index
 from .views.login import login
 from .views.contact import contact
-from .views.get_started import get_started
+from .views.sign_up import sign_up
 
 urlpatterns = [
     path('', index, name='home'),
     path('login/', login, name='login'),
     path('contact/', contact, name='contact'),
-    path('get-started/', get_started, name='get-started'),
+    path('sign-up/', sign_up, name='sign-up'),
 ]
--- etsywindow/web/views/sign_up.py
+++ etsywindow/web/views/sign_up.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from ..models import User
 
 
-def get_started(request):
+def sign_up(request):
     if request.method == 'POST':
     # create a User instance with the validated form data
         user = User(
@@ -11,16 +11,30 @@ def get_started(request):
             last_name=request.POST['last-name'],
             email_address=request.POST['email'],
         )
+        print("\n### HERE ###")
+        print("email:", user.first_name)
+        print("email:", user.last_name)
+        print("email:", user.email_address)
+        if not user.email_address:
+            # TODO: Force email required on form
+            messages.error(request, "Email is required")
+            context = {
+                'first_name': request.POST.get('first-name', ''),
+                'last_name': request.POST.get('last-name', ''),
+                'email_address': '',
+            }
+            return render(request, 'sign_up.html', context=context)
 
         # set and validate the password
         password = request.POST['password']
         password_verify = request.POST['password-verify']
 
         if password != password_verify:
+            # TODO: Force password required on form
             # TODO: Add a toast pop up for the user
             messages.error(request, "Passwords do not match")
             # TODO: Redirect but keep form populated
-            return redirect('get-started')
+            return redirect('sign-up')
 
         # TODO: Need to figure out this password stuff
         #user.password(password)
@@ -33,4 +47,5 @@ def get_started(request):
         return redirect('home') # TODO Need to get this to a different portal page
     else:
         # TODO: If all goes well then redirect to the portal as a logged in user
-        return render(request, 'get_started.html')
+        #return redirect('home') # TODO Need to get this to a different portal page
+        return render(request, 'sign_up.html')
