cebd2cebbdc6064000937ca2adb8eece0d7b2ae8
njmaeys <nmaeys@hotmail.com>
2023-04-16 10:13:47 -0500

    paginator next x pages not working

--- etsywindow/web/views/manage_store.py
+++ etsywindow/web/views/manage_store.py
@@ -17,7 +17,8 @@ def manage_store(request, store_id):
     listings = Listing.objects.filter(store_id=store.id)
 
     # Setup the paginator
-    paginator = Paginator(listings, 12)
+    #paginator = Paginator(listings, 12)
+    paginator = Paginator(listings, 3)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
